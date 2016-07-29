ee44d2c38e8f4eb9e9d7dda7d5bd865f61cc1925
Bruno Fernando dos Santos Silva <bruno2fernando@gmail.com>
2016-07-29 12:31:19 -0300

    Improve searchform

--- searchform.php
+++ searchform.php
@@ -1,4 +1,7 @@
-@extends('layouts.base')
-@section('content')
-    @include('contents.searchform')
-@endsection
+<form role="search" method="get" id="searchform"
+    class="searchform" action="{{ esc_url( home_url( '/' ) ) }}">
+    <div>
+        <input type="text" value="<?php echo get_search_query(); ?>" name="s" id="s" />
+        <input type="submit" id="searchsubmit" value="Buscar" />
+    </div>
+</form>
