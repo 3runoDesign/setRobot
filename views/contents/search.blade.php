04c5fc7caac34ad387ea9f21a9eb965a1543c57e
João Tiago <jtiagodca@gmail.com>
2016-08-05 14:54:11 +0100

    Página de busca

--- views/contents/search.blade.php
+++ views/contents/search.blade.php
@@ -1,1 +1,23 @@
-Busca
+<div>
+    <form id="domain-searchform" action="{{esc_url(home_url('/'))}}">
+        <input type="search" name="s" value="<?php echo get_search_query(); ?>">
+        <input type="submit">
+    </form>
+</div>
+<div>
+    <h2>Resultados Busca</h2>
+    <?php
+        if ( have_posts() ) : while ( have_posts() ) : the_post() ?>
+            <a href=" <?php the_permalink(); ?> "> <h2><?php  the_title(); ?></h2></a>
+            <p><?php the_excerpt(); ?></p>
+            <hr>
+    <?php
+        endwhile;
+            if( function_exists('wp_pagenavi') ) {
+                wp_pagenavi();
+            }
+    ?>
+    <?php else : ?>
+        @include('contents/404')
+    <?php endif; ?>
+</div>
