e1dcc6b356da46dbe947ce17defac43faa6dc85e
João Tiago <jtiagodca@gmail.com>
2016-08-06 14:46:35 +0100

    Search

--- views/contents/search.blade.php
+++ views/contents/search.blade.php
@@ -1,23 +1,18 @@
+{{ get_search_form() }}
 <div>
-    <form id="domain-searchform" action="{{esc_url(home_url('/'))}}">
-        <input type="search" name="s" value="<?php echo get_search_query(); ?>">
-        <input type="submit">
-    </form>
-</div>
-<div>
-    <h2>Resultados Busca</h2>
-    <?php
-        if ( have_posts() ) : while ( have_posts() ) : the_post() ?>
-            <a href=" <?php the_permalink(); ?> "> <h2><?php  the_title(); ?></h2></a>
-            <p><?php the_excerpt(); ?></p>
+   <h1>Resultados Busca</h1>
+
+   <ul>
+   @wpquery(have_posts())
+        <li>
+            <a href=" {{ the_permalink() }} ">
+                <h2>{{ the_title() }}</h2>
+                <p>{{ the_excerpt() }}</p>
+            </a>
             <hr>
-    <?php
-        endwhile;
-            if( function_exists('wp_pagenavi') ) {
-                wp_pagenavi();
-            }
-    ?>
-    <?php else : ?>
-        @include('contents/404')
-    <?php endif; ?>
+        </li>
+   @wpempty
+        <li>Sem Resultado!</li>
+   @wpend
+   </ul>
 </div>
