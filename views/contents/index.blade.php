0d83b4800a9e0ca2c6191740e990a52788015e29
Bruno Fernando dos Santos Silva <bruno2fernando@gmail.com>
2016-08-08 04:25:31 -0300

    Improve index

--- views/contents/index.blade.php
+++ views/contents/index.blade.php
@@ -4,7 +4,7 @@
         <img src="http://agenciaccm.s3-sa-east-1.amazonaws.com/uploads/2016/07/16165134/setrobot_logo.svg" alt="setRobot - logo" width="800px" />
 
         <p>
-            Olá, sou <strong>setRobot</strong>, <a href="https://github.com/3runoDesign/setRobot">veja a documentação</a>!
+            Olá, sou <strong>setRobot</strong>! <a href="https://github.com/3runoDesign/setRobot">Veja a documentação</a>!
         </p>
 
     </div>
