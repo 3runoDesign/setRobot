fc0e18e177f47d3b6374d72748a4f168d9713ed7
Bruno Fernando dos Santos Silva <bruno2fernando@gmail.com>
2016-08-08 03:44:59 -0300

    Add page

--- views/contents/page.blade.php
+++ views/contents/page.blade.php
@@ -1,1 +1,14 @@
-Page
+@while (have_posts()) {{ the_post() }}
+    <article {{ post_class() }}>
+        <header>
+          <h1 class="entry-title">
+              {{ the_title() }}
+          </h1>
+        </header>
+
+        <section class="entry-content">
+          {{ the_content() }}
+        </section>
+
+    </article>
+@endwhile
