b50fa8292f3760a147bcec70173efe4d279eec9d
Bruno Fernando dos Santos Silva <bruno2fernando@gmail.com>
2016-09-07 23:59:17 -0300

    Fix code style

--- resources/scripts/main.js
+++ resources/scripts/main.js
@@ -1,37 +1,40 @@
+'use strict';
+
 (function($) {
-    var routesApp = {
-        // All pages
-        common: {
-            init: function() {
-            }
-        },
-        // Home page
-        home: {
-            init: function() {
-            // JavaScript to be fired on the home page
-            }
-        }
-    };
-
-    // The routing fires all common scripts, followed by the page specific scripts.
-    // Add additional events for more control over timing e.g. a finalize event
-    var UTIL = {
-      fire: function(func, funcname, args) {
-        var namespace = routesApp;
-        funcname = (funcname === undefined) ? 'init' : funcname;
-        if (func !== '' && namespace[func] && typeof namespace[func][funcname] === 'function') {
-          namespace[func][funcname](args);
-        }
-      },
-      loadEvents: function() {
-        UTIL.fire('common');
-
-        $.each(document.body.className.replace(/-/g, '_').split(/\s+/),function(i,classnm) {
-          UTIL.fire(classnm);
-        });
+  var routesApp = {
+    // All pages
+    common: {
+      init: function() {
+        console.log('oi');
       }
-    };
+    },
+    // Home page
+    home: {
+      init: function() {
+      // JavaScript to be fired on the home page
+      }
+    }
+  };
 
-    $(document).ready(UTIL.loadEvents);
+  // The routing fires all common scripts, followed by the page specific scripts.
+  // Add additional events for more control over timing e.g. a finalize event
+  var UTIL = {
+    fire: function(func, funcname, args) {
+      var namespace = routesApp;
+      funcname = (funcname === undefined) ? 'init' : funcname;
+      if (func !== '' && namespace[func] && typeof namespace[func][funcname] === 'function') {
+        namespace[func][funcname](args);
+      }
+    },
+    loadEvents: function() {
+      UTIL.fire('common');
+
+      $.each(document.body.className.replace(/-/g, '_').split(/\s+/), function(i, classnm) {
+        UTIL.fire(classnm);
+      });
+    }
+  };
+
+  $(document).ready(UTIL.loadEvents);
 
 })(jQuery);
