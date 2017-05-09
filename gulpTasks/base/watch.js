00b80be93f4be0da1afc54d9840395dbc758aa6c
Bruno Fernando dos Santos Silva <bruno2fernando@gmail.com>
2017-05-09 18:31:24 -0300

    Add task to watch pictures

--- gulpTasks/base/watch.js
+++ gulpTasks/base/watch.js
@@ -4,6 +4,7 @@ var gulp = require('gulp');
 var path = require('../paths.js');
 
 gulp.task('watch', function () {
+  gulp.watch([path.to.images.source, path.to.images.sprite], ['basic-sprite']);
   gulp.watch(path.to.sass.source, ['basic-sass']);
   gulp.watch(path.to.scripts.source, ['scripts']);
   gulp.watch(path.to.fonts.source, ['fonts']);
