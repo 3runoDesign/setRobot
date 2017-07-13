68d185d49945ace926ddc4aeebeadaa3befe7e3f
Lucas de Souza Alves <lucasouzawe@hotmail.com>
2017-07-13 11:25:59 -0300

    add soberwp/controller

---
+++ resources/controllers/App.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Sober\Controller\Controller;
+
+class App extends Controller
+{
+    public function setDescription()
+    {
+        return 'Tema base para Wordpress com fluxo de desenvolvimento e <br> front-end moderno.';
+    }
+}
--- resources/views/front-page.blade.php
+++ resources/views/front-page.blade.php
@@ -5,7 +5,7 @@
         <section class="container">
             <div class="hero-text">
                 <h1>
-                    Tema base para Wordpress com fluxo de desenvolvimento e <br>front-end moderno.
+                    {!! App::setDescription() !!}
                 </h1>
             </div>
         </section>
