7c7fe4f0c2476f301fe0828922f87737824d1a8f
Bruno Fernando <bruno2fernando@gmail.com>
2017-07-17 15:35:29 -0300

    Ignore SVG in rev

--- gulpTasks/tasks/rev/rev-assets.js
+++ gulpTasks/tasks/rev/rev-assets.js
@@ -8,7 +8,7 @@ var revNapkin = require('gulp-rev-napkin');
 
 gulp.task('rev-assets', function () {
   // Ignore files that may reference assets. We'll rev them next.
-  var ignoreThese = '!' + path.join(config.buildPath, '/**/*+(css|js|json|html|ttf|eot|woff)');
+  var ignoreThese = '!' + path.join(config.buildPath, '/**/*+(css|js|json|html|ttf|eot|woff|svg)');
 
   return gulp.src([path.join(config.buildPath, '/**/*'), ignoreThese])
     .pipe(rev())
