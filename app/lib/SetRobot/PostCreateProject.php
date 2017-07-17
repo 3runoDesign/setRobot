156fa19d9404a7bccc849dc12a53edbef08d569a
Bruno Fernando <bruno2fernando@gmail.com>
2017-07-17 16:27:01 -0300

    Improve updateHeaders

--- app/lib/SetRobot/PostCreateProject.php
+++ app/lib/SetRobot/PostCreateProject.php
@@ -11,6 +11,7 @@ class PostCreateProject
     {
         // @codingStandardsIgnoreStart
         $io = $event->getIO();
+
         if ($io->isInteractive()) {
             $io->write('<info>Define theme headers. Press enter key for default.</info>');
             $theme_headers_default = [
@@ -41,11 +42,11 @@ class PostCreateProject
       $io->write('<info>Configure build settings. Press enter key for default.</info>');
 
       $browsersync_settings_default = [
-        'proxy'        => 'setrobot.dev'
+        'proxy' => 'setrobot.dev'
       ];
 
       $browsersync_settings = [
-        'proxy'        => $io->ask('<info>Local development URL of WP site [<comment>'.$browsersync_settings_default['proxy'].'</comment>]:</info>', $browsersync_settings_default['proxy'])
+        'proxy' => $io->ask('<info>Local development URL of WP site [<comment>'.$browsersync_settings_default['proxy'].'</comment>]:</info>', $browsersync_settings_default['proxy'])
       ];
 
       file_put_contents('gulpTasks/config/browserSync.js', str_replace($browsersync_settings_default['proxy'], $browsersync_settings['proxy'], file_get_contents('gulpTasks/config/browserSync.js')));
