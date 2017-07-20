a36ebf49f6c30686241e6803fb249243161ef8bd
Bruno Fernando <bruno2fernando@gmail.com>
2017-07-20 03:21:31 -0300

    Remove gulp-rigger

--- gulpTasks/tasks/scripts.js
+++ gulpTasks/tasks/scripts.js
@@ -1,12 +1,10 @@
 'use strict';
 
 var gulp = require('gulp');
-var rigger = require('gulp-rigger');
 
 var config = require('../config/scripts');
 
 gulp.task('scripts', function () {
   return gulp.src(config.source)
-    .pipe(rigger())
     .pipe(gulp.dest(config.dest));
 });
--- gulpTasks/tasks/scriptsProduction.js
+++ gulpTasks/tasks/scriptsProduction.js
@@ -3,13 +3,11 @@
 var buffer = require('vinyl-buffer');
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
-var rigger = require('gulp-rigger');
 
 var config = require('../config/scripts');
 
 gulp.task('scripts:production', function (done) {
   return gulp.src(config.source)
-    .pipe(rigger())
     .pipe(uglify())
     .pipe(buffer())
     .pipe(gulp.dest(config.dest));
