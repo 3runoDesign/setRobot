b369fb9cd22a31acc14dbd6a6b86b57ba1037b60
Lucas de Souza <lucasouzaweb@hotmail.com>
2017-07-31 14:43:09 -0300

    add missing font ext.

--- gulpTasks/config/fonts.js
+++ gulpTasks/config/fonts.js
@@ -3,6 +3,6 @@
 var config = require('./');
 
 module.exports = {
-  source: config.assetsPath + 'fonts/**/*.{eot,svg,ttf,woff,woff2}',
+  source: config.assetsPath + 'fonts/**/*.{eot,svg,ttf,otf,woff,woff2}',
   dest: config.buildPath + 'assets/fonts/'
 };
