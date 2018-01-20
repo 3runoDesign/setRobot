bedc937c3ed317b6a4d4959187d20219bec8e75e
Bruno Fernando <bruno2fernando@gmail.com>
2018-01-20 02:02:42 -0300

    Improve code

--- resources/views/template-custom.blade.php
+++ resources/views/template-custom.blade.php
@@ -5,8 +5,8 @@
 @extends('layouts.app')
 
 @section('content')
-  @while(have_posts()) @php(the_post())
-    @include('partials.page-header')
-    @include('partials.content-page')
-  @endwhile
+    @while(have_posts()) @php(the_post())
+        @include('partials.page-header')
+        @include('partials.content-page')
+    @endwhile
 @endsection
