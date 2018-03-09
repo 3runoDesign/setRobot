a8419c27025d60982ff471ba5648faa2049df160
Bruno Fernando dos Santos Silva <bruno2fernando@gmail.com>
2018-03-09 18:40:16 -0300

    Add Fields

---
+++ app/Fields/sample.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Fields;
+
+use StoutLogic\AcfBuilder\FieldsBuilder;
+
+/*
+ * ACF Builder documentation: https://github.com/StoutLogic/acf-builder/wiki
+ * ACF PHP documentation: https://www.advancedcustomfields.com/resources/register-fields-via-php/
+ * Guide to registering ACF fields via PHP: https://bwap.ch/guide-to-registering-acf-fields-via-php/
+ */
+/*
+/** Field Group setup */
+
+$sample_field = new FieldsBuilder('sample_field', [
+    'style' => 'seamless',
+    'position' => 'acf_after_title',
+    'hide_on_screen' => ['the_content'],
+]);
+
+/** Fields setup */
+$sample_field
+    ->addText('title')
+    ->addWysiwyg('content');
+
+/** Field Group location setup */
+$sample_field
+    ->setLocation('post_type', '==', 'post');
+
+return $sample_field;
