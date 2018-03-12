77681bedcc70f20eeb001ab80adf4909851d8e61
Bruno Fernando <bruno2fernando@gmail.com>
2018-03-12 01:52:39 -0300

    Improve code

--- app/admin.php
+++ app/admin.php
@@ -25,7 +25,20 @@ if (defined('WP_ENV')) {
         ]);
     }, 999);
     $adminbarCustomStylFn = function () {
-        echo '<style>#wpadminbar .admin-bar__current-env .ab-item{color:#f4f3f4!important;background-color: #fe3055!important}#wpadminbar .admin-bar__current-env[class*="is-staging"] .ab-item{background-color: #ffd84d!important}#wpadminbar .admin-bar__current-env[class*="is-dev"] .ab-item{background-color: #20be51!important}</style>';
+        echo '<style>
+            #wpadminbar .admin-bar__current-env .ab-item{
+                color:#f4f3f4!important;
+                background-color: #fe3055!important
+            }
+            
+            #wpadminbar .admin-bar__current-env[class*="is-staging"] .ab-item{
+                background-color: #ffd84d!important
+            }
+            
+            #wpadminbar .admin-bar__current-env[class*="is-production"] .ab-item{
+                background-color: #20be51!important
+            }
+        </style>';
     };
     add_action('admin_head', $adminbarCustomStylFn);
     if (!is_admin() && is_user_logged_in()) {
