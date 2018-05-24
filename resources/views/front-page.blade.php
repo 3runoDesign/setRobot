675befcc10e22bf100aa4ae7e6617d1442fff73a
Bruno Fernando dos Santos Silva <bruno2fernando@gmail.com>
2018-05-24 09:21:40 -0300

    Remove notification component on front page

--- resources/views/front-page.blade.php
+++ resources/views/front-page.blade.php
@@ -1,11 +1,8 @@
 @extends('layouts.app')
 
 @section('notification')
-    @component('components.notification-area', ['class' => 'success', 'time' => 2000])
-        {{ __('Logged in successfully!', 'setrobot') }}
-    @endcomponent
 @endsection
 
 @section('content')
-<my-teste></my-teste>
+    <my-teste></my-teste>
 @endsection
