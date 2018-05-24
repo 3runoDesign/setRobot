cd41b2ae6213d9517aef31060b08e3af998901c2
Bruno Fernando dos Santos Silva <bruno2fernando@gmail.com>
2018-05-24 09:33:37 -0300

    Improve js

--- resources/assets/scripts/app.js
+++ resources/assets/scripts/app.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 
-import Notification from '@vizuaalog/bulmajs/src/plugins/notification';
+import { Notification } from '@vizuaalog/bulmajs/src/plugins/notification';
 
 /**
  * First we will load all of this project's JavaScript dependencies which
